1b81d222c97c00d5fc4faa430bd991edcca5b291
bochill <dev.hafidhf@gmail.com>
2022-06-17 14:21:43 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve) => {
+    let total = 0;
+    promiseTheaterIXX()
+      .then((hasilNonton) => {
+        hasilNonton.forEach((key) => {
+          if (key.hasil == emosi) {
+            total += 1;
+          }
+        });
+      })
+      .then(
+        promiseTheaterVGC().then((hasilNonton) => {
+          hasilNonton.forEach((key) => {
+            if (key.hasil == emosi) {
+              total += 1;
+            }
+          });
+          resolve(total);
+        })
+      );
+  });
+};
 
 module.exports = {
   promiseOutput,
